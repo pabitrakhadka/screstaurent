c235b14990f4daaa9e770733b24d6f427dab9d41
pabitra <pabitrakhadka424@gmail.com>
2023-10-01 11:04:17 +0545

    auth

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import multer from "multer";
 
 // Define the destination directory for uploads
-const uploadDirectory = "/uploads";
+const uploadDirectory = "/tmp/uploads";
 const storage = multer.diskStorage({
     destination: uploadDirectory,
     filename: (req, file, cb) => {
