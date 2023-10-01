e65ca81c5a14d4e10e8cda31b7b3ba44cfa017c9
pabitra <pabitrakhadka424@gmail.com>
2023-10-01 10:36:21 +0545

    auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,40 @@
+// import axios from "axios";
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+
+// export default NextAuth({
+//   pages: {
+//     signIn: "/login",
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       async authorize(credentials, req) {
+//         try {
+//           console.log("loging");
+//           const { email, password } = credentials;
+//           const res = await axios.post('https://screstaurent.vercel.app/login', { email, password });
+
+//           if (res.data.status) {
+//             console.log("res:", res.data.result);
+//             const user = res.data.result;
+//             user.email = user.result[0].user_id;
+//             user.name = user.result[0].user_name;
+            
+//             return user;
+
+//           } else {
+//             return null;
+//           }
+//         } catch (error) {
+//           console.error(error);
+//           console.log("catch error");
+
+//         }
+//       }
+//     })
+//   ]
+// })
 import axios from "axios";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -11,16 +48,17 @@ export default NextAuth({
       name: "Credentials",
       async authorize(credentials, req) {
         try {
-          console.log("loging");
           const { email, password } = credentials;
-          const res = await axios.post('https://screstaurent.vercel.app/login', { email, password });
+          const res = await axios.post('https://screstaurent.vercel.app/api/login', { email, password });
 
           if (res.data.status) {
-            console.log("res:", res.data.result);
+           console.log("res:", res.data.result);
             const user = res.data.result;
             user.email = user.result[0].user_id;
             user.name = user.result[0].user_name;
-            
+            // console.log("id =", user.email, "name=", user.name);
+            // user.email = res.data.result[0].user_id;
+            // user.name = res.data.result[0].user_name;
             return user;
 
           } else {
