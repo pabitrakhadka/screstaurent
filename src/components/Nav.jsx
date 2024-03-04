1a4fc2a97d93e578d6df1b5f2bace902464d45d1
pabitra <pabitrakhadka424@gmail.com>
2024-03-04 20:26:10 +0545

    prisima update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
 
         <div className="container-fluid">
           <button
-            className="navbar-toggler-white"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarTogglerDemo01"
